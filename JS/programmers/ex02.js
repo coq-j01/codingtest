1b83a8bdaedc2173381d8aa27b7e8ba4f87d01fc
coq-j01 <chaebinesxo@gmail.com>
2025-05-12 11:31:17 +0900

    a와 b 출력하기

---
+++ JS/programmers/ex02.js
@@ -0,0 +1,14 @@
+// a와 b 출력하기
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log(`a = ${input[0]}\nb = ${input[1]}`);
+});
